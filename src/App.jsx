4973ea69891869b664d9a8b9ced7aa1b11cab838
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-10-22 19:26:42 +0200

    feature(home): add first structure folders & components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import { About } from './components/About'
+import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
+import { Hero } from './components/Hero'
+import { Navbar } from './components/Navbar'
+import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
 
 export const App = () => {
   return (
     <div>
-      <h1>Portfolio</h1>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   )
 }
