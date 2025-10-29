2e36fa704b32f66d1780fe87475758dd8086f1c1
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-10-29 13:13:15 +0100

    refactor(navbar): new design for navbar

--- src/components/Sidebar/SidebarHeader.jsx
+++
@@ -1,8 +0,0 @@
-export const SidebarHeader = () => {
-	return (
-		<div>
-			<h1 className="text-2xl font-bold text-accent mb-2">Raquel Ruz</h1>
-			<p className="text-lg text-text/70">Desarrolladora Web Full Stack</p>
-		</div>
-	);
-};
